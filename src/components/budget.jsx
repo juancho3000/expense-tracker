040996b0b14ab20d9c691a18675df4b7b3f9dc07
juan-suarez <jsuarez0698@gmail.com>
2022-09-19 13:07:28 -0400

    fixing the budget total with reduce

--- src/components/budget.jsx
+++ src/components/budget.jsx
@@ -2,11 +2,14 @@ import React, {useContext} from "react";
 import { ExpenseContext } from "./ExpenseContext";
 
 const Budget = () => {
-    const{budget} = useContext(ExpenseContext)
+   
+    const{expenses}= useContext(ExpenseContext);
+    const amounts = expenses.map(expenses=>expenses.cost);
+    const totalMoney = amounts.reduce((total, item)=>(total += item.cost),).toFixed(2);
     
     return(
         <div className="expense-money">
-            <span>Budget: ${budget}</span>
+            <span>Budget: ${totalMoney}</span>
         </div>
     )
 }
